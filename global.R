6638404e323686f3744078d85251d53516d5175d
Coquito <moises.e01@gmail.com>
2016-01-29 08:47:19 -0800

    added global file for shiny apps

---
+++ global.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(RCurl)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(scales)
+library(data.table)
+library(RColorBrewer)
+library(benford.analysis)
+library(mapproj)
+library(DT)
+
+## This code loads in the data. the data is in the data file
+# globla.r
+
+comboCities <-  fread("./data/comboCities.txt",
+                      sep = "\t",
+                      showProgress = FALSE)
+
+frtfCaCounties <-  fread("./data/frtfCaCounties.txt",
+                         sep = "\t",
+                         showProgress = FALSE)
+
+suspects <-  fread("./data/suspects.txt",
+                   sep = "\t",
+                   showProgress = FALSE)
+
+colourCount = length(unique(comboCities$Type))
+getPalette = colorRampPalette(brewer.pal(8, "Accent"))
+
+YCrimes <- select(comboCities, ends_with("Rate")) %>% names()
